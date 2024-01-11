a769d204375218b9ec11fd7f46f37cae06b29c16
MohibShaikh <mohibuddin9@gmail.com>
2024-01-11 07:58:54 +0500

    Form logic progression

--- MindMendConsultant/module1/admin.py
+++ MindMendConsultant/module1/admin.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
-from .models import Patient, Therapist, Sessions, PatientFeedback, TherapistFeedback, BookedSession, ComplainForm, \
+from .models import Patient, Therapist, Sessions, Feedback, BookedSession, ComplainForm, \
     CustomerReport, Notification
 
 admin.site.register(Patient)
 admin.site.register(Therapist)
 admin.site.register(Sessions)
-admin.site.register(PatientFeedback)
-admin.site.register(TherapistFeedback)
+admin.site.register(Feedback)
 admin.site.register(BookedSession)
 admin.site.register(ComplainForm)
 admin.site.register(CustomerReport)
